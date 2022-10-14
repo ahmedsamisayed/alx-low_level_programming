77d7443e66d5e9e6f35527c5dbd85a5c0bdeb3ba
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-14 11:32:30 -0700

    c code to compare

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'code to determine wether the random number sign and compare'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{int n, digit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+digit = n % 10;
+if (digit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+else
+if (digit < 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+else
+if (n == 0)
+printf("Last digit of %d is %d and is 0\n", n, digit);
+return (0);
+}
