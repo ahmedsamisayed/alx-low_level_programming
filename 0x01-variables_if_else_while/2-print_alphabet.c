f6fdfe4d0c712265ddc85022ee42158de19a9825
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-14 12:11:44 -0700

    c prog for print alpha lowwer case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'code to print lower case alpha'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{char x;
+char y = '\n';
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
+putchar(y);
+return (0);
+}
