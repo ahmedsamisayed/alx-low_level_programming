2b8e9b3457d664ba0b13e124e420294822c13e38
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-18 11:25:17 -0700

    get the absolute of the number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - get the absolute value
+ *@a: number to get absolute value to
+ *
+ * Return: result = 1 if letter is lower, else 0
+ */
+int _abs(int a)
+{
+int x = abs(a);
+return (x);
+}
