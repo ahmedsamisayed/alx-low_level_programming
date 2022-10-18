3f2d761f40db76dcc6fb25e7e17fe2783531b4b1
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-18 15:31:27 -0700

    function to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * add - add two numbers
+ *@a: the first number
+ *@b: the second number
+ *
+ * Return: result = 1 if letter is lower, else 0
+ */
+int add(int a, int b)
+{
+int x = a + b;
+return (x);
+}
