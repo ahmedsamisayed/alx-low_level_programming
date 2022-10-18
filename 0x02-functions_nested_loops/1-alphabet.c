79970fa0ff4459554f80a53d6d2662daa7e42525
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-18 06:22:14 -0700

    function to print all alphabet loweer case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabet in lowercase
+ *
+ * Return: no return value
+ */
+void print_alphabet(void)
+{
+char x[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i <= 25; i++)
+_putchar(x[i]);
+_putchar('\n');
+}
