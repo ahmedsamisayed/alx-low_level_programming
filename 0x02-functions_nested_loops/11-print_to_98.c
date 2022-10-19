9dde30517e2e8973e07616554cb35621c448c6db
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-19 01:15:52 -0700

    function to count to 98 from n number 2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * print_to_98 - print from starting to 98
  *@n: starting to printing
@@ -8,35 +8,16 @@
  */
 void print_to_98(int n)
 {
-int i;
-if (n < 98)
+if (n >= 98)
 {
-for (i = n; i <= 98; i++)
-{
-print(i);
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-}
-else
-if (n == 98)
-{
-print(n);
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
 }
 else
 {
-for (i = n; i >= 98; i--)
-{
-print(i);
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
 }
-}
-}
-_putchar('\n');
 }
