bd7676cf99234dd7b31a9f40381f1d17de1691a0
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-20 08:23:12 -0700

    print numbers 2

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,13 @@
 /**
  * print_numbers - print numbers from 0 to 9
  *
- * Return: Mutiply result between a and b
+ * Return:no return
  */
 void print_numbers(void)
 {
 	int x;
 
-	for(x = 0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 		_putchar(x + '0');
 	_putchar('\n');
 }
