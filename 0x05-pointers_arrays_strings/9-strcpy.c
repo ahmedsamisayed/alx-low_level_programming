615c4d23018cc98c54b56128ded3cea10c08c003
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-24 12:31:07 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _strcpy - Copies a string pointed to by @src, including the
- * 	     terminating null byte, to a buffer pointed to by @dest.
+ *	terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
  *
@@ -13,11 +13,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	for (; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
-		index++;
 	}
-	
+
+	dest[i++] = '\0';
+
 	return (dest);
 }
