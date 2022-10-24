e8321cf52ef1a6edcb38857bffb4e7ca14465478
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-24 09:31:36 -0700

    swap two integars values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values between two pointers
+ * @a: the first pointer
+ * @b: the second pointer
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tempror;
+
+	*a = tempror;
+	*a = *b;
+	*b = tempror;
+}
