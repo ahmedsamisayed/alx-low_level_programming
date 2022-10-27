c0d93e0d7309cc2bb1a314d5c3343ac72db222e1
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-27 07:29:07 -0700

    append string to another

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the src string to the dest string
+ * @dest: the first String
+ * @src: the second String
+ *
+ * Return: appended dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, dest_length = 0;
+
+	while (src[i++])
+		dest_length++;
+
+	for (i = 0; src[i]; i++)
+		dest[dest_length++] = src[i];
+
+	return (dest);
+}
