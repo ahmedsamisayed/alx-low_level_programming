cf3c1725b3c57cb5fac57b0d5a945bf5c7f01236
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-10-27 15:47:25 -0700

    encode string with leet encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - code a string with leet code
+ * @s: the string
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	int i1 = 0, i2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (s[i1])
+	{
+		for (i2 = 0; i2 <= 7; i2++)
+		{
+			if (s[i1] == leet[i2] || s[i1] - 32 == leet[i2])
+				s[i1] = i2 + '0';
+		}
+
+		i1++;
+	}
+
+	return (s);
+}
