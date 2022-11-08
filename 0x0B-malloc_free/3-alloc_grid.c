f8303a66d046e8a7fb6848f7e37b4f85608884c2
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-11-08 12:54:19 -0800

    function to create array of 0 members with given width and height

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: the width of the array
+ * @height: the height of the array
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ * else a pointer to the 2-dimensional array of integers.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	int a[width][height], i, j;
+
+	a = malloc(sizeof(int) * width * height);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+
+	return (a);
+}
