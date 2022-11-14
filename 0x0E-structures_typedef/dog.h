c9c77e2d3805d03d441e057f5e721768b0a0fc30
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-11-14 12:38:54 -0800

    structure for dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+
+/**
+ * struct dog - for dogs
+ * @name: name of the dog element
+ * @age: age of the dog element
+ * @owner: owner of the dog element
+ *
+ * Description: structure for any type of dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* STRUCT_DOG_H */
