e5cea6c2decfecf0b04c096f1700f78c0362ab84
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-11-14 11:44:51 -0800

    prgram the file name after compiled

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file after compiled
+ *
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
