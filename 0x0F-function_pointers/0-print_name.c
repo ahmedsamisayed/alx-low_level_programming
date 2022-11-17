54c95d0f0fe72476c0c2375b6a5e97e55478afe9
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-11-17 06:47:28 -0800

    function to print name with callback for other function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name: prints a name.
+ * @name: the name to print
+ * @f: pointer of function to call back
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
 
 #endif /* FUNCTION_POINTERS_H */
