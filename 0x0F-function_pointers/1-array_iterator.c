725910c95973485ea54d83c207ca4f09dbec4570
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-11-17 07:06:02 -0800

    function to apllay specific action on array elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given
+ * as a parameter on each element of an array.
+ * @array: array of elemnts to apply action on
+ * @size: size of the array
+ * @action: pointer of function to call back
+ *
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || size == NULL || action == NULL)
+		return;
+
+	unsigned long int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
