f035237e23c55eddaee93f51a31a93dbaf3fa1f2
ahmedsamisayed <ahmedsamisayed1@gmail.com>
2022-12-05 13:10:26 -0800

    a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,104 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *cr_buf(char *file);
+void cl_file(int fd);
+
+/**
+ * cr_buf - ..
+ * @file: ..
+ *
+ * Return: ..
+ */
+
+char *cr_buf(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * cl_file - ..
+ * @fd: ..
+ *
+ * Return: ..
+ */
+
+void cl_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - ..
+ * @argc: ..
+ * @argv: ..
+ *
+ * Return: ..
+ *
+ * Description: ..
+ */
+
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = cr_buf(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	cl_file(from);
+	cl_file(to);
+
+	return (0);
+}
